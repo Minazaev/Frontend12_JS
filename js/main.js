ed96e082d837d5286c516370347149bb6c8d75af
Tamerlan <minazaev3256@gmail.com>
2023-07-31 17:56:16 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,58 +1,32 @@
-const a = 5;
-
-if(a < 10){
-    console.log("Значение меньше 10")
-}else{
-    console.log("Константа не меньше 10")
+function a(x = 11){
+    if(x < 10){
+        console.log("константа меньше 10")
+    }else{
+        console.log("константа не меньше 10")
+    }
 }
 
-let login = 'Usman'
-let password = '1234'
+a();
 
-let loginUser = prompt('login?')
-let passwordUser = prompt('password?')
-
-if(login == loginUser && password == passwordUser){
-    console.log("welcome")
-}else{
-    console.log("Неправильный логин или пароль")
+function hello(){
+    alert("Hello world!")
 }
 
-let x = 10;
-let y = 7;
+hello();
 
-let c = x > y ? 'x больше чем y' : 'х меньше чем у'
-
-console.log(c)
-
-let num = prompt("Введите число")
-
-if(num % 2 === 0){
-    console.log("Число " +num+ " четное")
-}else{
-    console.log("Число " +num+ " нечетное")
+function js(){
+    console.log("JavaScript")
 }
 
-let num1 = prompt("Первое число")
-let num2 = prompt("Второе число")
-let num3 = prompt("Третье число")
+js();
 
-if(num1 > num2 && num1 > num3){
-    console.log("Максимальное число " +num1)
-}
-else if(num2 > num1 && num2 > num3){
-    console.log("Максимальное число " +num2)
-}else if(num3 > num2 && num3 > num1){
-    console.log("Максимальное число " +num3)
+function text(){
+    let text = prompt("Введите текст")
+    if(text === "lorem"){
+        console.log("У вас ошибка в тексте")
+    }else{
+        console.log(text)
+    }
 }
 
-
-// if(num1 === Math.max){
-//     console.log("fds")
-// }
-// else if(num2 === Math.max){
-//     console.log("2")
-// }
-// else{
-//     console.log("3")
-// }
+text();
