c3cd2cba4f01216e8312c2ca01c694fd2e198fba
Tamerlan <minazaev3256@gmail.com>
2023-08-18 10:18:58 +0300

    practice

--- Массивы/main.js
+++ Массивы/main.js
@@ -1,75 +1,126 @@
-//Первое задание
-let array = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100]
-array.forEach(function(item){
-    if(item % 2 !== 0){
-        console.log(item)
-    }
-})
-// второе задание
-let array2 = ["lorem ipsum", "javascript", "php", "css", "react", "git"]
-array2.forEach(function(item){
-    if(item.length > 4){
-        console.log(item)
-    }
-})
+const cities = [
+    {name: 'Москва', population: 12506468},
+    {name: 'Санкт-Петербург', population: 5351935},
+    {name: 'Новосибирск', population: 1612833},
+    {name: 'Калиниград', population: 482443},        
+    {name: 'Калуга', population: 336726}
+];
 
-// третье задание
-let array3 = [1, -3, 5, 6, -7, 8, -9, -11]
+cities.unshift({name: 'Грозный', population: 1234311})
+cities.push({name: 'Магас', population: 12332411})
 
-let array4 = array3.filter(function(item){
-    if(item < 0){
-        console.log(item)
-    }
+console.log(cities)
+cities.forEach(function(item){
+
+    console.log("город: " +item.name, "население: " +item.population)
 })
 
-// четвертое задание
-let array5 = [10, 2762, 0.42, 192, 3, 28, 1125, 2018, 34, 66, 100] 
-let array6 = array5.filter(function(item){
-    if(item % 2 == 0){
-        console.log(item)
-    }
-})
-
-// пятое задание
-let array7 = ["lorem ipsum", "javascript", "php", "css", "react", "git"]
-let array8 = array7.filter(function(item){
-    if(item.length !== 3){
+let array2 = cities.filter(function(item){
+    if(item.population > 1000000){
         return item;
     }
 })
-console.log(array8)
 
-// шестое задание
-let array9 = [5, 6, 7, 8, 9]
+console.log(array2)
 
-let array10 = array9.map(function(item){
-    return item * item
+let array3 = cities.map(function(item){
+    return item.population * 1.5;
 })
-console.log(array10)
 
-// седьмое задание
-let array11 = [{a: 10, b: 5}, {a: 20, b: 22}, {a: 131, b: 55}]
+console.log(array3)
 
-let array12 = array11.map(function(sum){
-    return sum.a + sum.b
+let array4 = array3.reduce((sum, population) => sum + population, 0);
+console.log(array4);
+
+array5 = [
+    {
+        "userId": 1,
+        "id": 1,
+        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+      },
+      {
+        "userId": 1,
+        "id": 2,
+        "title": "qui est esse",
+        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+      },
+      {
+        "userId": 1,
+        "id": 3,
+        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+      },
+      {
+        "userId": 1,
+        "id": 4,
+        "title": "eum et est occaecati",
+        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+      },
+      {
+        "userId": 1,
+        "id": 5,
+        "title": "nesciunt quas odio",
+        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
+      },
+      {
+        "userId": 1,
+        "id": 6,
+        "title": "dolorem eum magni eos aperiam quia",
+        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
+      },
+      {
+        "userId": 1,
+        "id": 7,
+        "title": "magnam facilis autem",
+        "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
+      },
+      {
+        "userId": 1,
+        "id": 8,
+        "title": "dolorem dolore est ipsam",
+        "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
+      },
+      {
+        "userId": 1,
+        "id": 9,
+        "title": "nesciunt iure omnis dolorem tempora et accusantium",
+        "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
+      },
+      {
+        "userId": 1,
+        "id": 10,
+        "title": "optio molestias id quia eum",
+        "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
+      }
+]
+
+let array6 = array5.filter(function(item){
+    if(item.id > 5){
+        return item
+    } 
 })
-console.log(array12)
 
-// восьмое задание
-let array13 = [-13, 0, 12, 1662, -0.32, -102, -2]
-let array14 = array13.filter(function(item){
-    return item < 0;
+console.log(array6)
+
+array5.forEach(function(item){
+  console.log("title: " +item.title)
+  console.log("body: " +item.body)
 })
-console.log(array14)
 
-let array15 = array14.reduce(function(sum, current){
-    return sum + current;
+let array7 = array5.map(function(item){
+  return item.title = 'expo'
 })
-console.log(array15)
 
-// девятое задание
-let array16 = [{x: 10, y: "lorem"}, {x: 21, y: "lorem"}, {x: -17, y: "lorem"}, {x: 156, y: "lorem"}];
-let array17 = array16.reduce(function(sum, current){
-    return sum + current.x;
-}, 0)
-console.log(array17)
+console.log(array7)
+console.log(array5)
+
+
+
+
+
+
+
+
+
+
