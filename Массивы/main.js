ab785be16df38eafbe1b1b9306a44a86a0cdab1c
Tamerlan <minazaev3256@gmail.com>
2023-08-21 11:38:44 +0300

    rest

--- Массивы/main.js
+++ Массивы/main.js
@@ -1,120 +1,38 @@
-const cities = [
-    {name: 'Москва', population: 12506468},
-    {name: 'Санкт-Петербург', population: 5351935},
-    {name: 'Новосибирск', population: 1612833},
-    {name: 'Калиниград', population: 482443},        
-    {name: 'Калуга', population: 336726}
-];
+arr_1 = [101, 202, 303, 404, 505]
+arr_2 = [606, 707, 808, 909]
 
-cities.unshift({name: 'Грозный', population: 1234311})
-cities.push({name: 'Магас', population: 12332411})
+superArr = [...arr_1, ...arr_2]
 
-console.log(cities)
-cities.forEach(function(item){
+console.log(...superArr)
 
-    console.log("город: " +item.name, "население: " +item.population)
-})
+console.log(Math.min(...superArr))
 
-let array2 = cities.filter(function(item){
-    if(item.population > 1000000){
-        return item;
-    }
-})
+let obj = {
+  width: 300,
+  height: 550
+}
 
-console.log(array2)
+console.log(obj)
 
-let array3 = cities.map(function(item){
-    return item.population * 1.5;
-})
+let obj2 = {
+  ...obj,
+  area(){
+    return this.width * this.height;
+  }
+};
 
-console.log(array3)
+obj2.area()
+console.log(obj2)
 
-let array4 = array3.reduce((sum, population) => sum + population, 0);
-console.log(array4);
 
-array5 = [
-    {
-        "userId": 1,
-        "id": 1,
-        "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
-        "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
-      },
-      {
-        "userId": 1,
-        "id": 2,
-        "title": "qui est esse",
-        "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
-      },
-      {
-        "userId": 1,
-        "id": 3,
-        "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
-        "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
-      },
-      {
-        "userId": 1,
-        "id": 4,
-        "title": "eum et est occaecati",
-        "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
-      },
-      {
-        "userId": 1,
-        "id": 5,
-        "title": "nesciunt quas odio",
-        "body": "repudiandae veniam quaerat sunt sed\nalias aut fugiat sit autem sed est\nvoluptatem omnis possimus esse voluptatibus quis\nest aut tenetur dolor neque"
-      },
-      {
-        "userId": 1,
-        "id": 6,
-        "title": "dolorem eum magni eos aperiam quia",
-        "body": "ut aspernatur corporis harum nihil quis provident sequi\nmollitia nobis aliquid molestiae\nperspiciatis et ea nemo ab reprehenderit accusantium quas\nvoluptate dolores velit et doloremque molestiae"
-      },
-      {
-        "userId": 1,
-        "id": 7,
-        "title": "magnam facilis autem",
-        "body": "dolore placeat quibusdam ea quo vitae\nmagni quis enim qui quis quo nemo aut saepe\nquidem repellat excepturi ut quia\nsunt ut sequi eos ea sed quas"
-      },
-      {
-        "userId": 1,
-        "id": 8,
-        "title": "dolorem dolore est ipsam",
-        "body": "dignissimos aperiam dolorem qui eum\nfacilis quibusdam animi sint suscipit qui sint possimus cum\nquaerat magni maiores excepturi\nipsam ut commodi dolor voluptatum modi aut vitae"
-      },
-      {
-        "userId": 1,
-        "id": 9,
-        "title": "nesciunt iure omnis dolorem tempora et accusantium",
-        "body": "consectetur animi nesciunt iure dolore\nenim quia ad\nveniam autem ut quam aut nobis\net est aut quod aut provident voluptas autem voluptas"
-      },
-      {
-        "userId": 1,
-        "id": 10,
-        "title": "optio molestias id quia eum",
-        "body": "quo et expedita modi cum officia vel magni\ndoloribus qui repudiandae\nvero nisi sit\nquos veniam quod sed accusamus veritatis error"
-      }
-]
+function func(...rest){
+  arr5 = rest.reduce(function(x, rest){
+    return x + rest
+  }, 0)
+}
 
-let array6 = array5.filter(function(item){
-    if(item.id > 5){
-        return item
-    } 
-})
-
-console.log(array6)
-
-array5.forEach(function(item){
-  console.log("title: " +item.title)
-  console.log("body: " +item.body)
-})
-
-let array7 = array5.map(function(item){
-  return item.title = 'expo'
-})
-
-console.log(array7)
-console.log(array5)
-
+func(19, 29, 2, 2, 2)
+console.log(arr5)
 
 
 
