68117398614129e89e7adc1110d10ea8aba64196
Tamerlan <minazaev3256@gmail.com>
2023-08-24 10:56:44 +0300

    dom

--- Массивы/main.js
+++ Массивы/main.js
@@ -1,38 +1,38 @@
-arr_1 = [101, 202, 303, 404, 505]
-arr_2 = [606, 707, 808, 909]
+// arr_1 = [101, 202, 303, 404, 505]
+// arr_2 = [606, 707, 808, 909]
 
-superArr = [...arr_1, ...arr_2]
+// superArr = [...arr_1, ...arr_2]
 
-console.log(...superArr)
+// console.log(...superArr)
 
-console.log(Math.min(...superArr))
+// console.log(Math.min(...superArr))
 
-let obj = {
-  width: 300,
-  height: 550
-}
+// let obj = {
+//   width: 300,
+//   height: 550
+// }
 
-console.log(obj)
+// console.log(obj)
 
-let obj2 = {
-  ...obj,
-  area(){
-    return this.width * this.height;
-  }
-};
+// let obj2 = {
+//   ...obj,
+//   area(){
+//     return this.width * this.height;
+//   }
+// };
 
-obj2.area()
-console.log(obj2)
+// obj2.area()
+// console.log(obj2)
 
 
-function func(...rest){
-  arr5 = rest.reduce(function(x, rest){
-    return x + rest
-  }, 0)
-}
+// function func(...rest){
+//   arr5 = rest.reduce(function(x, rest){
+//     return x + rest
+//   }, 0)
+// }
 
-func(19, 29, 2, 2, 2)
-console.log(arr5)
+// func(19, 29, 2, 2, 2)
+// console.log(arr5)
 
 
 
